aae192ce9d2e1b1d633791098a5163d5c4396061
Nguyenyenbg <nguyenyenbg018@gmail.com>
2023-10-12 08:05:57 +0700

    respon tablet

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ var swiper = new Swiper('.js_swiper', {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 32,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.35,
+      spaceBetween: 32,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
 });
